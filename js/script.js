090ccd29536e09c56268212cad090446d0301022
Isaiah <id2coleman@icloud.com>
2023-01-12 12:20:13 -0800

    Prompt for user input

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
+let userInput = prompt('Enter # of rows and columns')
 const container = document.querySelector("#container");
 
-for (let i = 1; i <= 16; i++) { //Loop that creates columns and rows based on user input
+for (let i = 1; i <= userInput; i++) { //Loop that creates columns and rows based on user input
     const row = document.createElement('div'); 
     row.classList.value = (`row ${[i]}`);
     container.appendChild(row);
 
-    for (let i = 1; i <= 16; i++) {
+    for (let i = 1; i <= userInput; i++) {
         const unit = document.createElement('div');
         unit.classList.value = (`unit ${[i]}`);
         row.appendChild(unit);
