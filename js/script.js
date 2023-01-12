28da29d79230b2b7ed9ffa4d2f5e1e361bda7b72
Isaiah <id2coleman@icloud.com>
2023-01-12 12:37:06 -0800

    Fix borders

--- js/script.js
+++ js/script.js
@@ -6,10 +6,20 @@ for (let i = 1; i <= userInput; i++) { //Loop that creates columns and rows base
     row.classList.value = (`row ${[i]}`);
     container.appendChild(row);
 
+    if (i === (userInput - 1) + 1) {
+        row.style.border = "0px";
+    }
+
     for (let i = 1; i <= userInput; i++) {
         const unit = document.createElement('div');
         unit.classList.value = (`unit ${[i]}`);
         row.appendChild(unit);
+        
+        if (i === (userInput - 1) + 1) {
+            unit.style.border = "0px";
+        }
     }
+
+
 }
 
